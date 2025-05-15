2b30c2c409e662da0ab68e0727c8199f35da5fde
chauthutran <cthutran@gmail.com>
2025-05-15 11:37:24 +0900

    For testing

--- app/components/sectionHeader/Header.tsx
+++ app/components/sectionHeader/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
         <div className="flex items-center gap-2">
           <h1 className="text-lg font-semibold">PSI DHIS2 Approval</h1>
         </div>
-
+sfasdf
         <div className="flex items-center space-x-4">
             <span className="text-sm">JC</span>
             <div className="relative w-8 h-8 bg-gray-400 rounded-full flex items-center justify-center">J</div>
